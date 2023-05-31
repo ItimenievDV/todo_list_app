778c82533c3e45aef53c405df596b8174637b937
ItimenievDV <dima.itimenev@gmail.com>
2023-05-31 18:50:16 +0300

    added new functionality: 1. Title 2. Description 3. Image uploading 4. Editing tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:image_picker/image_picker.dart';
 
-void main() {
-  runApp(const TodoListApp());
-}
+void main() => runApp(const TodoApp());
 
-class TodoListApp extends StatelessWidget {
-  const TodoListApp({super.key});
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo List',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const TodoListScreen(),
     );
   }
@@ -30,8 +28,8 @@ class TodoListScreen extends StatefulWidget {
 class _TodoListScreenState extends State<TodoListScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-  final List<String> _todoList = [];
-  final List<String> _completedList = [];
+  List<TodoItem> todoItems = [];
+  List<TodoItem> completedItems = [];
 
   @override
   void initState() {
@@ -45,206 +43,330 @@ class _TodoListScreenState extends State<TodoListScreen>
     super.dispose();
   }
 
-  void _moveToCompleted(int index) {
+  void markAsDone(TodoItem item) {
     setState(() {
-      String task = _todoList.removeAt(index);
-      _completedList.add(task);
+      todoItems.remove(item);
+      completedItems.add(item);
     });
   }
 
-  void _moveToTodoList(int index) {
+  void moveToTodoList(TodoItem item) {
     setState(() {
-      String task = _completedList.removeAt(index);
-      _todoList.add(task);
+      completedItems.remove(item);
+      todoItems.add(item);
     });
   }
 
-  void _deleteTask(int index) {
+  void deleteItem(TodoItem item) {
     setState(() {
-      _todoList.removeAt(index);
+      if (todoItems.contains(item)) {
+        todoItems.remove(item);
+      } else if (completedItems.contains(item)) {
+        completedItems.remove(item);
+      }
     });
   }
 
-  Future<DateTime?> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    );
-    return picked;
+  void addNewTodoItem(String title, String description, File? image) {
+    setState(() {
+      todoItems.add(TodoItem(title, description, image: image));
+    });
   }
 
-  Widget _buildTaskItem(String task, int index) {
-    return Slidable(
-      endActionPane: ActionPane(
-        motion: const BehindMotion(),
-        children: [
-          SlidableAction(
-            onPressed: (context) {
-              _moveToCompleted(index);
-            },
-            backgroundColor: Colors.green,
-            icon: Icons.done,
-            label: 'Done',
-          ),
-          SlidableAction(
-            onPressed: (context) {},
-            backgroundColor: Colors.grey,
-            icon: Icons.edit,
-            label: 'Edit',
-          ),
-          SlidableAction(
-            onPressed: (context) {
-              _deleteTask(index);
-            },
-            backgroundColor: Colors.red,
-            icon: Icons.delete,
-            label: 'Delete',
-          )
-        ],
-      ),
-      child: Card(
-        child: ListTile(
-          title: Text(_todoList[index]),
-          // subtitle: Text('Date: ${_selectDate(context).toString()}'),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildDoneTaskItem(String task, int index) {
-    return Slidable(
-      endActionPane: ActionPane(
-        motion: const BehindMotion(),
-        children: [
-          SlidableAction(
-            onPressed: (context) {
-              _moveToTodoList(index);
-            },
-            backgroundColor: Colors.blue,
-            icon: Icons.undo,
-            label: 'Undo',
-          )
-        ],
-      ),
-      child: Card(
-        child: ListTile(title: Text(_completedList[index])),
-        // subtitle: Text('Date: ${task.date.toString()}'),
-      ),
-    );
-  }
-
-  Widget _buildTodoTaskList(List<String> taskList) {
-    return ListView.builder(
-      itemCount: taskList.length,
-      itemBuilder: (context, index) {
-        return _buildTaskItem(taskList[index], index);
-      },
-    );
-  }
-
-  Widget _buildDoneTaskList(List<String> taskList) {
-    return ListView.builder(
-      itemCount: taskList.length,
-      itemBuilder: (context, index) {
-        return _buildDoneTaskItem(taskList[index], index);
-      },
-    );
+  void updateTodoItem(
+      TodoItem item, String newTitle, String newDescription, File? newImage) {
+    setState(() {
+      item.title = newTitle;
+      item.description = newDescription;
+      item.image = newImage;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.purple[200],
-      appBar: AppBar(
-        backgroundColor: Colors.purple[400],
-        title: const Text('Todo List'),
-        centerTitle: true,
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const [
-            Tab(text: 'To do'),
-            Tab(text: 'Done'),
-          ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: Colors.purple[200],
+        appBar: AppBar(
+          backgroundColor: Colors.purple[400],
+          title: const Text('Todo List'),
+          centerTitle: true,
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: const [
+              Tab(text: 'To do'),
+              Tab(text: 'Done'),
+            ],
+          ),
         ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          _buildTodoTaskList(_todoList),
-          _buildDoneTaskList(_completedList),
-        ],
-      ),
-      floatingActionButton: _tabController.index == 0
-          ? FloatingActionButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    TextEditingController textEditingController =
-                        TextEditingController();
-                    DateTime? selectedDate;
-                    return AlertDialog(
-                      title: const Text('Add task'),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          TextField(
-                            controller: textEditingController,
-                            decoration: const InputDecoration(
-                              labelText: 'write here',
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          Row(
-                            children: [
-                              const Text('Date:'),
-                              Text(
-                                selectedDate != null
-                                    ? selectedDate.toString()
-                                    : 'Chose date',
-                              ),
-                              IconButton(
-                                icon: const Icon(Icons.calendar_today),
-                                onPressed: () async {
-                                  DateTime? date = await _selectDate(context);
-                                  setState(() {
-                                    selectedDate = date;
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
-                        ],
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            ListView.builder(
+              itemCount: todoItems.length,
+              itemBuilder: (BuildContext context, int index) {
+                TodoItem item = todoItems[index];
+                return Slidable(
+                  endActionPane: ActionPane(
+                    motion: const BehindMotion(),
+                    children: [
+                      SlidableAction(
+                        label: 'Done',
+                        backgroundColor: Colors.green,
+                        icon: Icons.done,
+                        onPressed: (context) {
+                          markAsDone(item);
+                        },
                       ),
-                      actions: [
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text('Cancel'),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            if (textEditingController.text.isNotEmpty &&
-                                selectedDate != null) {
-                              setState(() {
-                                _todoList.add(textEditingController.text);
-                              });
-                              Navigator.pop(context);
-                            }
-                          },
-                          child: const Text('Add'),
-                        ),
-                      ],
-                    );
-                  },
+                      SlidableAction(
+                        label: 'Edit',
+                        backgroundColor: Colors.grey,
+                        icon: Icons.edit,
+                        onPressed: (context) {
+                          final picker = ImagePicker();
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              TextEditingController titleController =
+                                  TextEditingController(text: item.title);
+                              TextEditingController descriptionController =
+                                  TextEditingController(text: item.description);
+                              File? newImage;
+                              Future<void> getImage() async {
+                                final pickedFile = await picker.pickImage(
+                                    source: ImageSource.gallery);
+                                if (pickedFile != null) {
+                                  setState(() {
+                                    newImage = File(pickedFile.path);
+                                  });
+                                }
+                              }
+
+                              return AlertDialog(
+                                title: const Text('Edit task'),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    TextField(
+                                      controller: titleController,
+                                      decoration: const InputDecoration(
+                                        labelText: 'Title',
+                                      ),
+                                    ),
+                                    TextField(
+                                      controller: descriptionController,
+                                      decoration: const InputDecoration(
+                                        labelText: 'Description',
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: getImage,
+                                    child: const Text('Change image'),
+                                  ),
+                                  TextButton(
+                                    child: const Text('Cancel'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                  TextButton(
+                                    child: const Text('Save'),
+                                    onPressed: () {
+                                      String newTitle = titleController.text;
+                                      String newDescription =
+                                          descriptionController.text;
+                                      updateTodoItem(item, newTitle,
+                                          newDescription, newImage);
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                      ),
+                      SlidableAction(
+                        label: 'Delete',
+                        backgroundColor: Colors.red,
+                        icon: Icons.delete,
+                        onPressed: (context) {
+                          deleteItem(item);
+                        },
+                      )
+                    ],
+                  ),
+                  child: Card(
+                    child: ListTile(
+                      title: Text(item.title),
+                      subtitle: Text(item.description),
+                      leading: item.getImageWidget(),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TodoDetailScreen(item),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 );
               },
-              child: const Icon(Icons.add),
-            )
-          : null,
+            ),
+            ListView.builder(
+              itemCount: completedItems.length,
+              itemBuilder: (BuildContext context, int index) {
+                TodoItem item = completedItems[index];
+                return Slidable(
+                  endActionPane: ActionPane(
+                    motion: const BehindMotion(),
+                    children: [
+                      SlidableAction(
+                        onPressed: (context) {
+                          moveToTodoList(item);
+                        },
+                        backgroundColor: Colors.blue,
+                        icon: Icons.undo,
+                        label: 'Undo',
+                      )
+                    ],
+                  ),
+                  child: Card(
+                    child: ListTile(
+                        title: Text(item.title),
+                        subtitle: Text(item.description),
+                        leading: item.getImageWidget()),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+        floatingActionButton: _tabController.index == 0
+            ? FloatingActionButton(
+                onPressed: () {
+                  final picker = ImagePicker();
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      TextEditingController titleController =
+                          TextEditingController();
+                      TextEditingController descriptionController =
+                          TextEditingController();
+                      File? image;
+
+                      Future<void> getImage() async {
+                        final pickedFile =
+                            await picker.pickImage(source: ImageSource.gallery);
+                        if (pickedFile != null) {
+                          setState(() {
+                            image = File(pickedFile.path);
+                          });
+                        }
+                      }
+
+                      return AlertDialog(
+                        title: const Text('Add task'),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            if (image != null) Image.file(image!),
+                            ElevatedButton(
+                              onPressed: getImage,
+                              child: const Text('Upload image'),
+                            ),
+                            TextField(
+                              controller: titleController,
+                              decoration: const InputDecoration(
+                                labelText: 'Title',
+                              ),
+                            ),
+                            TextField(
+                              controller: descriptionController,
+                              decoration: const InputDecoration(
+                                labelText: 'Description',
+                              ),
+                            ),
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                            child: const Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            child: const Text('Add'),
+                            onPressed: () {
+                              String title = titleController.text;
+                              String description = descriptionController.text;
+                              addNewTodoItem(title, description, image);
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Icon(Icons.add),
+              )
+            : null,
+      ),
     );
+  }
+}
+
+class TodoDetailScreen extends StatelessWidget {
+  final TodoItem item;
+
+  const TodoDetailScreen(this.item, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Task description')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (item.image != null) Image.file(item.image!),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              item.title,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              item.description,
+              style: const TextStyle(fontSize: 18),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TodoItem {
+  String title;
+  String description;
+  File? image;
+
+  TodoItem(this.title, this.description, {this.image});
+
+  Widget getImageWidget() {
+    if (image != null) {
+      return Image.file(image!);
+    }
+    return const SizedBox.shrink();
   }
 }
